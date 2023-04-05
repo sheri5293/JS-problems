5f24d79ff022c02ebbc746840a87c1578bf5828a
sheri5293 <ssheheryar12@gmail.com>
2023-04-05 19:53:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,724 @@
+//Program to find prime and composite number
+/*let num = 10;
+var isPrimeNumber = true;
+
+for(let i = 2; i < num; i++){
+    if( num % i == 0){
+        isPrimeNumber = false;
+    }
+}
+if(isPrimeNumber == true){
+    console.log(`${num} is a prime number`);
+}else{
+    console.log(`${num} is not a prime number`);
+}*/
+
+// reverse an array in javascript.
+
+//let array = [1,2,3,4,5];
+//type1
+/*modifying the source array
+let reversedArray = array.reverse();
+console.log('type1',reversedArray);*/
+
+//type2 for loop
+/*let reversedArray =[];
+for(let i = array.length; i>=0; i--){
+   reversedArray.push(array[i]);
+}
+console.log('type2', reversedArray);*/
+
+//type3
+/*let reversedArray = array.slice(0 , array.length).reverse();
+console.log('type3' , reversedArray);*/
+
+
+//length 
+var a = [10, 20, 30];
+console.log(a.length);
+//shift
+var sh = [10, 20, 30, 40];
+console.log(sh.shift()); //10
+console.log(sh);  //20,30,40
+
+var sha =[];
+console.log(sha.shift());
+
+//unshift
+var unsh = [10, 20, 30, 40];
+console.log(unsh.unshift('77'));
+console.log(unsh);
+
+//pop
+var p = [10, 20, 30, 40];
+console.log(p.pop());
+console.log(p);
+
+//push
+var pu = [10, 20, 30, 40];
+console.log(pu.push('50'));
+console.log(pu);
+
+//indexOf
+var i = [10, 20, 30, 40];
+console.log(i.indexOf(30));
+
+//lastindex of
+var li = [10, 20, 30, 40, 50, 60];
+console.log(li.lastIndexOf(30));
+
+//includes
+var inc = [10, 20, 30, 40, 50, 60];
+console.log(inc.includes(30));
+
+//join
+var jo = [10, 20, 30, 40, 50, 60];
+console.log(jo.join('-'));
+console.log(jo.join(''));
+
+//reverse
+var re = [10, 20, 30, 40, 50, 60];
+console.log(re.reverse());
+
+//concat
+var cnc = [10, 20, 30, 40, 50, 60];
+console.log(cnc.concat(70,80));
+
+//foreach
+var foaar = [10, 20, 30];
+foaar.forEach(function(value,index){
+    console.log(value);
+})
+
+// calculte the length of the array without using length method
+
+var a = [10 , 20 , 30 , 40 , 50];
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[5]);
+
+
+
+//Program to find prime and composite number
+/*let num = 10;
+var isPrimeNumber = true;
+
+for(let i = 2; i < num; i++){
+    if( num % i == 0){
+        isPrimeNumber = false;
+    }
+}
+if(isPrimeNumber == true){
+    console.log(`${num} is a prime number`);
+}else{
+    console.log(`${num} is not a prime number`);
+}*/
+
+// reverse an array in javascript.
+
+//let array = [1,2,3,4,5];
+//type1
+/*modifying the source array
+let reversedArray = array.reverse();
+console.log('type1',reversedArray);*/
+
+//type2 for loop
+/*let reversedArray =[];
+for(let i = array.length; i>=0; i--){
+   reversedArray.push(array[i]);
+}
+console.log('type2', reversedArray);*/
+
+//type3
+/*let reversedArray = array.slice(0 , array.length).reverse();
+console.log('type3' , reversedArray);*/
+
+
+//length 
+var a = [10, 20, 30];
+console.log(a.length);
+//shift
+var sh = [10, 20, 30, 40];
+console.log(sh.shift()); //10
+console.log(sh);  //20,30,40
+
+var sha =[];
+console.log(sha.shift());
+
+//unshift
+var unsh = [10, 20, 30, 40];
+console.log(unsh.unshift('77'));
+console.log(unsh);
+
+//pop
+var p = [10, 20, 30, 40];
+console.log(p.pop());
+console.log(p);
+
+//push
+var pu = [10, 20, 30, 40];
+console.log(pu.push('50'));
+console.log(pu);
+
+//indexOf
+var i = [10, 20, 30, 40];
+console.log(i.indexOf(30));
+
+//lastindex of
+var li = [10, 20, 30, 40, 50, 60];
+console.log(li.lastIndexOf(30));
+
+//includes
+var inc = [10, 20, 30, 40, 50, 60];
+console.log(inc.includes(30));
+
+//join
+var jo = [10, 20, 30, 40, 50, 60];
+console.log(jo.join('-'));
+console.log(jo.join(''));
+
+//reverse
+var re = [10, 20, 30, 40, 50, 60];
+console.log(re.reverse());
+
+//concat
+var cnc = [10, 20, 30, 40, 50, 60];
+console.log(cnc.concat(70,80));
+
+//foreach
+var foaar = [10, 20, 30];
+foaar.forEach(function(value,index){
+    console.log(value);
+})
+
+// calculte the length of the array without using length method
+
+var a = [10 , 20 , 30 , 40 , 50];
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[5]);
+
+
+
+
+//Program to find prime and composite number
+/*let num = 10;
+var isPrimeNumber = true;
+
+for(let i = 2; i < num; i++){
+    if( num % i == 0){
+        isPrimeNumber = false;
+    }
+}
+if(isPrimeNumber == true){
+    console.log(`${num} is a prime number`);
+}else{
+    console.log(`${num} is not a prime number`);
+}*/
+
+// reverse an array in javascript.
+
+//let array = [1,2,3,4,5];
+//type1
+/*modifying the source array
+let reversedArray = array.reverse();
+console.log('type1',reversedArray);*/
+
+//type2 for loop
+/*let reversedArray =[];
+for(let i = array.length; i>=0; i--){
+   reversedArray.push(array[i]);
+}
+console.log('type2', reversedArray);*/
+
+//type3
+/*let reversedArray = array.slice(0 , array.length).reverse();
+console.log('type3' , reversedArray);*/
+
+
+//length 
+var a = [10, 20, 30];
+console.log(a.length);
+//shift
+var sh = [10, 20, 30, 40];
+console.log(sh.shift()); //10
+console.log(sh);  //20,30,40
+
+var sha =[];
+console.log(sha.shift());
+
+//unshift
+var unsh = [10, 20, 30, 40];
+console.log(unsh.unshift('77'));
+console.log(unsh);
+
+//pop
+var p = [10, 20, 30, 40];
+console.log(p.pop());
+console.log(p);
+
+//push
+var pu = [10, 20, 30, 40];
+console.log(pu.push('50'));
+console.log(pu);
+
+//indexOf
+var i = [10, 20, 30, 40];
+console.log(i.indexOf(30));
+
+//lastindex of
+var li = [10, 20, 30, 40, 50, 60];
+console.log(li.lastIndexOf(30));
+
+//includes
+var inc = [10, 20, 30, 40, 50, 60];
+console.log(inc.includes(30));
+
+//join
+var jo = [10, 20, 30, 40, 50, 60];
+console.log(jo.join('-'));
+console.log(jo.join(''));
+
+//reverse
+var re = [10, 20, 30, 40, 50, 60];
+console.log(re.reverse());
+
+//concat
+var cnc = [10, 20, 30, 40, 50, 60];
+console.log(cnc.concat(70,80));
+
+//foreach
+var foaar = [10, 20, 30];
+foaar.forEach(function(value,index){
+    console.log(value);
+})
+
+// calculte the length of the array without using length method
+
+var a = [10 , 20 , 30 , 40 , 50];
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[5]);
+
+
+
+
+//Program to find prime and composite number
+/*let num = 10;
+var isPrimeNumber = true;
+
+for(let i = 2; i < num; i++){
+    if( num % i == 0){
+        isPrimeNumber = false;
+    }
+}
+if(isPrimeNumber == true){
+    console.log(`${num} is a prime number`);
+}else{
+    console.log(`${num} is not a prime number`);
+}*/
+
+// reverse an array in javascript.
+
+//let array = [1,2,3,4,5];
+//type1
+/*modifying the source array
+let reversedArray = array.reverse();
+console.log('type1',reversedArray);*/
+
+//type2 for loop
+/*let reversedArray =[];
+for(let i = array.length; i>=0; i--){
+   reversedArray.push(array[i]);
+}
+console.log('type2', reversedArray);*/
+
+//type3
+/*let reversedArray = array.slice(0 , array.length).reverse();
+console.log('type3' , reversedArray);*/
+
+
+//length 
+var a = [10, 20, 30];
+console.log(a.length);
+//shift
+var sh = [10, 20, 30, 40];
+console.log(sh.shift()); //10
+console.log(sh);  //20,30,40
+
+var sha =[];
+console.log(sha.shift());
+
+//unshift
+var unsh = [10, 20, 30, 40];
+console.log(unsh.unshift('77'));
+console.log(unsh);
+
+//pop
+var p = [10, 20, 30, 40];
+console.log(p.pop());
+console.log(p);
+
+//push
+var pu = [10, 20, 30, 40];
+console.log(pu.push('50'));
+console.log(pu);
+
+//indexOf
+var i = [10, 20, 30, 40];
+console.log(i.indexOf(30));
+
+//lastindex of
+var li = [10, 20, 30, 40, 50, 60];
+console.log(li.lastIndexOf(30));
+
+//includes
+var inc = [10, 20, 30, 40, 50, 60];
+console.log(inc.includes(30));
+
+//join
+var jo = [10, 20, 30, 40, 50, 60];
+console.log(jo.join('-'));
+console.log(jo.join(''));
+
+//reverse
+var re = [10, 20, 30, 40, 50, 60];
+console.log(re.reverse());
+
+//concat
+var cnc = [10, 20, 30, 40, 50, 60];
+console.log(cnc.concat(70,80));
+
+//foreach
+var foaar = [10, 20, 30];
+foaar.forEach(function(value,index){
+    console.log(value);
+})
+
+// calculte the length of the array without using length method
+
+var a = [10 , 20 , 30 , 40 , 50];
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[5]);
+
+
+
+
+//Program to find prime and composite number
+/*let num = 10;
+var isPrimeNumber = true;
+
+for(let i = 2; i < num; i++){
+    if( num % i == 0){
+        isPrimeNumber = false;
+    }
+}
+if(isPrimeNumber == true){
+    console.log(`${num} is a prime number`);
+}else{
+    console.log(`${num} is not a prime number`);
+}*/
+
+// reverse an array in javascript.
+
+//let array = [1,2,3,4,5];
+//type1
+/*modifying the source array
+let reversedArray = array.reverse();
+console.log('type1',reversedArray);*/
+
+//type2 for loop
+/*let reversedArray =[];
+for(let i = array.length; i>=0; i--){
+   reversedArray.push(array[i]);
+}
+console.log('type2', reversedArray);*/
+
+//type3
+/*let reversedArray = array.slice(0 , array.length).reverse();
+console.log('type3' , reversedArray);*/
+
+
+//length 
+var a = [10, 20, 30];
+console.log(a.length);
+//shift
+var sh = [10, 20, 30, 40];
+console.log(sh.shift()); //10
+console.log(sh);  //20,30,40
+
+var sha =[];
+console.log(sha.shift());
+
+//unshift
+var unsh = [10, 20, 30, 40];
+console.log(unsh.unshift('77'));
+console.log(unsh);
+
+//pop
+var p = [10, 20, 30, 40];
+console.log(p.pop());
+console.log(p);
+
+//push
+var pu = [10, 20, 30, 40];
+console.log(pu.push('50'));
+console.log(pu);
+
+//indexOf
+var i = [10, 20, 30, 40];
+console.log(i.indexOf(30));
+
+//lastindex of
+var li = [10, 20, 30, 40, 50, 60];
+console.log(li.lastIndexOf(30));
+
+//includes
+var inc = [10, 20, 30, 40, 50, 60];
+console.log(inc.includes(30));
+
+//join
+var jo = [10, 20, 30, 40, 50, 60];
+console.log(jo.join('-'));
+console.log(jo.join(''));
+
+//reverse
+var re = [10, 20, 30, 40, 50, 60];
+console.log(re.reverse());
+
+//concat
+var cnc = [10, 20, 30, 40, 50, 60];
+console.log(cnc.concat(70,80));
+
+//foreach
+var foaar = [10, 20, 30];
+foaar.forEach(function(value,index){
+    console.log(value);
+})
+
+// calculte the length of the array without using length method
+
+var a = [10 , 20 , 30 , 40 , 50];
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[5]);
+
+
+
+
+//Program to find prime and composite number
+/*let num = 10;
+var isPrimeNumber = true;
+
+for(let i = 2; i < num; i++){
+    if( num % i == 0){
+        isPrimeNumber = false;
+    }
+}
+if(isPrimeNumber == true){
+    console.log(`${num} is a prime number`);
+}else{
+    console.log(`${num} is not a prime number`);
+}*/
+
+// reverse an array in javascript.
+
+//let array = [1,2,3,4,5];
+//type1
+/*modifying the source array
+let reversedArray = array.reverse();
+console.log('type1',reversedArray);*/
+
+//type2 for loop
+/*let reversedArray =[];
+for(let i = array.length; i>=0; i--){
+   reversedArray.push(array[i]);
+}
+console.log('type2', reversedArray);*/
+
+//type3
+/*let reversedArray = array.slice(0 , array.length).reverse();
+console.log('type3' , reversedArray);*/
+
+
+//length 
+var a = [10, 20, 30];
+console.log(a.length);
+//shift
+var sh = [10, 20, 30, 40];
+console.log(sh.shift()); //10
+console.log(sh);  //20,30,40
+
+var sha =[];
+console.log(sha.shift());
+
+//unshift
+var unsh = [10, 20, 30, 40];
+console.log(unsh.unshift('77'));
+console.log(unsh);
+
+//pop
+var p = [10, 20, 30, 40];
+console.log(p.pop());
+console.log(p);
+
+//push
+var pu = [10, 20, 30, 40];
+console.log(pu.push('50'));
+console.log(pu);
+
+//indexOf
+var i = [10, 20, 30, 40];
+console.log(i.indexOf(30));
+
+//lastindex of
+var li = [10, 20, 30, 40, 50, 60];
+console.log(li.lastIndexOf(30));
+
+//includes
+var inc = [10, 20, 30, 40, 50, 60];
+console.log(inc.includes(30));
+
+//join
+var jo = [10, 20, 30, 40, 50, 60];
+console.log(jo.join('-'));
+console.log(jo.join(''));
+
+//reverse
+var re = [10, 20, 30, 40, 50, 60];
+console.log(re.reverse());
+
+//concat
+var cnc = [10, 20, 30, 40, 50, 60];
+console.log(cnc.concat(70,80));
+
+//foreach
+var foaar = [10, 20, 30];
+foaar.forEach(function(value,index){
+    console.log(value);
+})
+
+// calculte the length of the array without using length method
+
+var a = [10 , 20 , 30 , 40 , 50];
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[5]);
+
+
+
+
+//Program to find prime and composite number
+/*let num = 10;
+var isPrimeNumber = true;
+
+for(let i = 2; i < num; i++){
+    if( num % i == 0){
+        isPrimeNumber = false;
+    }
+}
+if(isPrimeNumber == true){
+    console.log(`${num} is a prime number`);
+}else{
+    console.log(`${num} is not a prime number`);
+}*/
+
+// reverse an array in javascript.
+
+//let array = [1,2,3,4,5];
+//type1
+/*modifying the source array
+let reversedArray = array.reverse();
+console.log('type1',reversedArray);*/
+
+//type2 for loop
+/*let reversedArray =[];
+for(let i = array.length; i>=0; i--){
+   reversedArray.push(array[i]);
+}
+console.log('type2', reversedArray);*/
+
+//type3
+/*let reversedArray = array.slice(0 , array.length).reverse();
+console.log('type3' , reversedArray);*/
+
+
+//length 
+var a = [10, 20, 30];
+console.log(a.length);
+//shift
+var sh = [10, 20, 30, 40];
+console.log(sh.shift()); //10
+console.log(sh);  //20,30,40
+
+var sha =[];
+console.log(sha.shift());
+
+//unshift
+var unsh = [10, 20, 30, 40];
+console.log(unsh.unshift('77'));
+console.log(unsh);
+
+//pop
+var p = [10, 20, 30, 40];
+console.log(p.pop());
+console.log(p);
+
+//push
+var pu = [10, 20, 30, 40];
+console.log(pu.push('50'));
+console.log(pu);
+
+//indexOf
+var i = [10, 20, 30, 40];
+console.log(i.indexOf(30));
+
+//lastindex of
+var li = [10, 20, 30, 40, 50, 60];
+console.log(li.lastIndexOf(30));
+
+//includes
+var inc = [10, 20, 30, 40, 50, 60];
+console.log(inc.includes(30));
+
+//join
+var jo = [10, 20, 30, 40, 50, 60];
+console.log(jo.join('-'));
+console.log(jo.join(''));
+
+//reverse
+var re = [10, 20, 30, 40, 50, 60];
+console.log(re.reverse());
+
+//concat
+var cnc = [10, 20, 30, 40, 50, 60];
+console.log(cnc.concat(70,80));
+
+//foreach
+var foaar = [10, 20, 30];
+foaar.forEach(function(value,index){
+    console.log(value);
+})
+
+// calculte the length of the array without using length method
+
+var a = [10 , 20 , 30 , 40 , 50];
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[5]);
+
+
+
+
+
+
+
+
